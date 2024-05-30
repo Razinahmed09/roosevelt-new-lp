ca8c497c88891d628038c4d9f31af35617a8a643
Razin Ahmed <raheenahmee@gmail.com>
2024-05-30 07:08:43 -0700

    complete without responsive

--- js/swiper.js
+++ js/swiper.js
@@ -9,3 +9,9 @@ document.addEventListener( 'DOMContentLoaded', function () {
   
 });
 
+function openCity(e, t) {
+  var o, n, s;
+  for (o = 0, n = document.getElementsByClassName("tabcontent"); o < n.length; o++) n[o].style.display = "none";
+  for (o = 0, s = document.getElementsByClassName("tablinks"); o < s.length; o++) s[o].className = s[o].className.replace(" active", "");
+  (document.getElementById(t).style.display = "block"), (e.currentTarget.className += " active");
+}
